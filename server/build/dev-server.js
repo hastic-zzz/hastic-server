683c25aedb15e559da844a1cd5991d22fefa2f12
rozetko <rozetko09@gmail.com>
2018-05-29 15:06:28 +0300

    nodemon stderr output

--- server/build/dev-server.js
+++ server/build/dev-server.js
@@ -4,7 +4,7 @@ const webpack = spawn('webpack', ['--config', 'build/webpack.dev.conf.js'], {
   stdio: 'inherit',
   shell: true
 });
-//webpack.stdout.pipe(process.stdout);
 
 const nodemon = spawn('nodemon', ['../dist/server', '--watch', 'server.js']);
 nodemon.stdout.pipe(process.stdout);
+nodemon.stderr.pipe(process.stderr);
