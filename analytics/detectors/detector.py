92828b0b6f76f8953737298cc15f1b53ed02a2d4
rozetko <rozetko@corpglory.com>
2018-08-30 21:46:22 +0300

    More review fixes

--- analytics/detectors/detector.py
+++ analytics/detectors/detector.py
@@ -1,3 +1,4 @@
+from models import AnalyticUnitCache
 from abc import ABC, abstractmethod
 from pandas import DataFrame
 from typing import Optional
@@ -6,9 +7,9 @@ from typing import Optional
 class Detector(ABC):
 
     @abstractmethod
-    async def train(self, dataframe: DataFrame, segments: list, cache: Optional[dict]) -> dict:
+    async def train(self, dataframe: DataFrame, segments: list, cache: Optional[AnalyticUnitCache]) -> AnalyticUnitCache:
         pass
 
     @abstractmethod
-    async def predict(self, dataframe: DataFrame, cache: Optional[dict]) -> dict:
+    async def predict(self, dataframe: DataFrame, cache: Optional[AnalyticUnitCache]) -> dict:
         pass
