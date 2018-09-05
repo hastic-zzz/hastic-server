edcf487479a2c3c398e6685cec550596da404060
Alexey Velikiy <gmpota@gmail.com>
2018-09-05 09:46:53 +0200

    Update custom_model.py

--- analytics/models/custom_model.py
+++ analytics/models/custom_model.py
@@ -3,15 +3,8 @@ import utils
 import pandas as pd
 from typing import Optional
 
-# Paste your model here:
-class CustomModel(Model):
 
-    def __init__(self):
-        super()
-        # Use self.state to store results of your learning
-        # It will be saved in filesystem and loaded after server restart
-        self.state = {}
-    
+class CustomModel(Model):    
     def fit(self, dataframe: pd.DataFrame, segments: list, cache: Optional[dict]) -> dict:
         pass
     
