3cbb2cebaa3645fe66545433758ef4ce751c7dcb
amper43 <amper43@corpglory.com>
2018-09-27 07:41:05 +0300

    fix imports

--- server/src/controllers/alerts_controller.ts
+++ server/src/controllers/alerts_controller.ts
@@ -1,72 +1,65 @@
-/**
- * Alarting is not supported yet
- */
+import { runPredict } from './analytics_controller';
+import * as Segment from '../models/segment_model';
+import { AnalyticUnitId } from '../models/analytic_unit_model';
+import { sendNotification } from '../services/notification_service';
+import { getJsonDataSync, writeJsonDataSync } from '../services/json_service';
+import { DATA_PATH } from '../config';
 
-throw new Error('not supported');
-
-
-// import { runPredict } from './analytics_controller';
-// import { getLabeledSegments } from './segments_controller';
-// import { AnalyticUnitId } from '../models/analytic_unit';
-// import { sendNotification } from '../services/notification_service';
-// import { getJsonDataSync, writeJsonDataSync } from '../services/json_service';
-// import { ANALYTIC_UNITS_PATH } from '../config';
-
-// import * as path from 'path';
-// import * as fs from 'fs';
+import * as path from 'path';
+import * as fs from 'fs';
 
 
-// const ALERT_TIMEOUT = 60000; // ms
-// const ALERTS_DB_PATH = path.join(ANALYTIC_UNITS_PATH, `alerts_anomalies.json`);
+const ALERT_TIMEOUT = 60000; // ms
+const ALERTS_DB_PATH = path.join(DATA_PATH, `alerts_anomalies.json`);
 
 
-// export function getAlertsAnomalies(): AnalyticUnitId[] {
-//   if(!fs.existsSync(ALERTS_DB_PATH)) {
-//     saveAlertsAnomalies([]);
-//   }
-//   return getJsonDataSync(ALERTS_DB_PATH);
-// }
+export function getAlertsAnomalies(): AnalyticUnitId[] {
+  if(!fs.existsSync(ALERTS_DB_PATH)) {
+    saveAlertsAnomalies([]);
+  }
+  return getJsonDataSync(ALERTS_DB_PATH);
+}
 
-// export function saveAlertsAnomalies(units: AnalyticUnitId[]) {
-//   return writeJsonDataSync(ALERTS_DB_PATH, units);
-// }
+export function saveAlertsAnomalies(units: AnalyticUnitId[]) {
+  return writeJsonDataSync(ALERTS_DB_PATH, units);
+}
 
-// function processAlerts(id: AnalyticUnitId) {
-//   let segments = getLabeledSegments(id);
+async function processAlerts(id: AnalyticUnitId) {
+  let segments = await Segment.findMany(id, { labeled: true });
 
-//   const currentTime = new Date().getTime();
-//   const activeAlert = activeAlerts.has(id);
-//   let newActiveAlert = false;
+  const currentTime = new Date().getTime();
+  const activeAlert = activeAlerts.has(id);
+  let newActiveAlert = false;
 
-//   if(segments.length > 0) {
-//     let lastSegment = segments[segments.length - 1];
-//     if(lastSegment.finish >= currentTime - ALERT_TIMEOUT) {
-//       newActiveAlert = true;
-//     }
-//   }
+  if(segments.length > 0) {
+    let lastSegment = segments[segments.length - 1];
+    if(lastSegment.to >= currentTime - ALERT_TIMEOUT) {
+      newActiveAlert = true;
+    }
+  }
 
-//   if(!activeAlert && newActiveAlert) {
-//     activeAlerts.add(id);
-//     sendNotification(id, true);
-//   } else if(activeAlert && !newActiveAlert) {
-//     activeAlerts.delete(id);
-//     sendNotification(id, false);
-//   }
-// }
+  if(!activeAlert && newActiveAlert) {
+    activeAlerts.add(id);
+    sendNotification(id, true);
+  } else if(activeAlert && !newActiveAlert) {
+    activeAlerts.delete(id);
+    sendNotification(id, false);
+  }
+}
 
-// async function alertsTick() {
-//   let alertsAnomalies = getAlertsAnomalies();
-//   for (let predictorId of alertsAnomalies) {
-//     try {
-//       await runPredict(predictorId);
-//       processAlerts(predictorId);
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   }
-//   setTimeout(alertsTick, 5000);
-// }
+async function alertsTick() {
+  let alertsAnomalies = getAlertsAnomalies();
+  for (let predictorId of alertsAnomalies) {
+    try {
+      await runPredict(predictorId);
+      processAlerts(predictorId);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  setTimeout(alertsTick, 5000);
+}
 
 
-// const activeAlerts = new Set<string>();
-// setTimeout(alertsTick, 5000);
+const activeAlerts = new Set<string>();
+setTimeout(alertsTick, 5000);
--- server/src/routes/alerts_router.ts
+++ server/src/routes/alerts_router.ts
@@ -1,43 +1,36 @@
-// /**
-//  * Alarting is not supported yet
-//  */
+import * as AnalyticUnit from '../models/analytic_unit_model';
+import { getAlertsAnomalies, saveAlertsAnomalies } from '../controllers/alerts_controller';
 
-// throw new console.error("Not supported");
-
-
-// import * as AnalyticUnit from '../models/analytic_unit';
-// import { getAlertsAnomalies, saveAlertsAnomalies } from '../controllers/alerts_controller';
-
-// import * as Router from 'koa-router';
+import * as Router from 'koa-router';
 
 
-// function getAlert(ctx: Router.IRouterContext) {
-//   let id: AnalyticUnit.AnalyticUnitId = ctx.request.query.id;
+function getAlert(ctx: Router.IRouterContext) {
+  let id: AnalyticUnit.AnalyticUnitId = ctx.request.query.id;
 
-//   let alertsAnomalies = getAlertsAnomalies();
-//   let pos = alertsAnomalies.indexOf(id);
+  let alertsAnomalies = getAlertsAnomalies();
+  let pos = alertsAnomalies.indexOf(id);
 
-//   let enabled: boolean = (pos !== -1);
-//   ctx.response.body = { enabled };
-// }
+  let enabled: boolean = (pos !== -1);
+  ctx.response.body = { enabled };
+}
 
-// function setAlertEnabled(ctx: Router.IRouterContext) {
-//   let id: AnalyticUnit.AnalyticUnitId = ctx.request.body.id;
-//   let enabled: boolean = ctx.request.body.enabled;
+function setAlertEnabled(ctx: Router.IRouterContext) {
+  let id: AnalyticUnit.AnalyticUnitId = ctx.request.body.id;
+  let enabled: boolean = ctx.request.body.enabled;
 
-//   let alertsAnomalies = getAlertsAnomalies();
-//   let pos: number = alertsAnomalies.indexOf(id);
-//   if(enabled && pos == -1) {
-//     alertsAnomalies.push(id);
-//     saveAlertsAnomalies(alertsAnomalies);
-//   } else if(!enabled && pos > -1) {
-//     alertsAnomalies.splice(pos, 1);
-//     saveAlertsAnomalies(alertsAnomalies);
-//   }
-//   ctx.response.body = { status: 'OK' };
-// }
+  let alertsAnomalies = getAlertsAnomalies();
+  let pos: number = alertsAnomalies.indexOf(id);
+  if(enabled && pos == -1) {
+    alertsAnomalies.push(id);
+    saveAlertsAnomalies(alertsAnomalies);
+  } else if(!enabled && pos > -1) {
+    alertsAnomalies.splice(pos, 1);
+    saveAlertsAnomalies(alertsAnomalies);
+  }
+  ctx.response.body = { status: 'OK' };
+}
 
-// export const router = new Router();
+export const router = new Router();
 
-// router.get('/', getAlert);
-// router.post('/', setAlertEnabled);
+router.get('/', getAlert);
+router.post('/', setAlertEnabled);
