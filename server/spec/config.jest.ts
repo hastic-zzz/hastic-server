47205b4283375a241b471bba1fb1c3f4325e7a30
amper43 <amper43@corpglory.com>
2018-10-29 15:53:42 +0300

    fix

--- server/spec/config.jest.ts
+++ server/spec/config.jest.ts
@@ -2,6 +2,6 @@ import { HASTIC_PORT }  from '../src/config';
 
 describe("When importing from .ts files", function() {
   it("config should be correct", function() {
-    expect(HASTIC_PORT).toBe(8000);
+    expect(HASTIC_PORT).toBe("8000");
   })
 })
--- server/spec/segments.jest.ts
+++ server/spec/segments.jest.ts
@@ -1,37 +1,39 @@
-//import {app} from '../src/index'
 import { deleteNonpredictedSegments } from '../src/controllers/analytics_controller'
 import * as AnalyticUnit from '../src/models/analytic_unit_model'
 import * as Segment from '../src/models/segment_model';
 import { Collection, makeDBQ } from '../src/services/data_service';
 
-jest.mock('../__mocks__/nedb');
+var id: AnalyticUnit.AnalyticUnitId = 'auid';
 
-//beforeAll(() => {
-//});
-
-//afterAll(() => {
-//});
-
+beforeAll(() => {
+  let times = [[0,1], [2,3], [4,5]];
+  let segments = times.map(t => {
+    return new Segment.Segment('auid',t[0], t[1],false,false); 
+  });
+  Segment.insertSegments(segments);
+});
 
 describe("Check deleted segments", function() {
-
-  let id: AnalyticUnit.AnalyticUnitId = 'auid';
-  //let db = makeDBQ(Collection.SEGMENTS);
+  let payload = {
+    lastPredictionTime: 0,
+    segments: [],
+    cache: null
+  }
 
   it("deleted should be empty", function() {
-    let payload = [];
+    payload.segments = [];
     
     let preSegments = Segment.findMany(id, {labeled: false, deleted:false});
-    deleteNonpredictedSegments(id, payload);
+    await deleteNonpredictedSegments(id, payload);
     let postSegments = Segment.findMany(id, {labeled: false, deleted:false});
-    let deleted = [];
+    let deleted = postSegments.remove() ;
     expect(deleted).toBe([]);
   });
 
   it("deleted should be non empty", function() {
-    let payload = [];
+    payload.segments = [];
     let preSegments = Segment.findMany(id, {labeled: false, deleted:false});
-    deleteNonpredictedSegments(id, payload);
+    await deleteNonpredictedSegments(id, payload);
     let postSegments = Segment.findMany(id, {labeled: false, deleted:false});
     let deleted = [];
     expect(deleted).toBe([]);
