10700776163f286dd104e91e7bfa8c029947b900
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-10-31 16:14:04 +0300

    minor fix

--- analytics/models/jump_model.py
+++ analytics/models/jump_model.py
@@ -157,7 +157,7 @@ class JumpModel(Model):
         delete_list = []
         variance_error = self.state['WINDOW_SIZE']
         close_patterns = utils.close_filtering(segments, variance_error)
-        segments = utils.best_pat(close_patterns, data, "max")
+        segments = utils.best_pat(close_patterns, data, 'max')
             
         if len(segments) == 0 or len(self.ijumps) == 0 :
             segments = []
