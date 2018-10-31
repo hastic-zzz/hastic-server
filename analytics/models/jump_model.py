ce53deca188e3356f5dc432b2179a4ec68e79a41
amper43 <amper43@corpglory.com>
2018-10-31 14:43:30 +0300

    fix

--- analytics/models/jump_model.py
+++ analytics/models/jump_model.py
@@ -1,6 +1,7 @@
 from models import Model
 
 import utils
+from utils.segments import parse_segment
 import numpy as np
 import pandas as pd
 import scipy.signal
@@ -37,7 +38,7 @@ class JumpModel(Model):
         patterns_list = []
         for segment in segments:
             if segment['labeled']:
-                segment_from_index, segment_to_index, segment_data = utils.parse_segment(segment, dataframe)
+                segment_from_index, segment_to_index, segment_data = parse_segment(segment, dataframe)
                 if len(segment_data) == 0:
                     continue    
                 segment_min = min(segment_data)
