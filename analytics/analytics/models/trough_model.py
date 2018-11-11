fc034376bcc657235d333c1b02db7577dff86df1
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-11-11 23:33:42 +0300

    trough fix

--- analytics/analytics/models/trough_model.py
+++ analytics/analytics/models/trough_model.py
@@ -133,7 +133,7 @@ class TroughModel(Model):
                     continue
                 elif 0 < percent_of_nans < 0.5:
                     nan_list = utils.nan_checker(convol_data)[1]
-                    pattern_data = utils.nan_for_zero(convol_data, nan_list)
+                    convol_data = utils.nan_for_zero(convol_data, nan_list)
                     pattern_data = utils.nan_for_zero(pattern_data, nan_list)
                 conv = scipy.signal.fftconvolve(convol_data, pattern_data)
                 if max(conv) > self.state['convolve_max'] * 1.1 or max(conv) < self.state['convolve_min'] * 0.9:
