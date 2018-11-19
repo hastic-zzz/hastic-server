b3e81d372c9b4e4fcef2b482e342510025a4795e
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-11-19 12:12:23 +0300

    get_convolve in utils

--- analytics/analytics/utils/segments.py
+++ analytics/analytics/utils/segments.py
@@ -7,3 +7,15 @@ def parse_segment(segment, dataframe):
     end = timestamp_to_index(dataframe, pd.to_datetime(segment['to'], unit='ms'))
     data = dataframe['value'][start: end + 1]
     return start, end, data
+
+def get_convolve(segments, av_model, data, window_size):
+    labeled_segment = []
+    convolve_list = []
+    for segment in segments:
+            labeled_segment = data[segment - window_size: segment + window_size + 1]
+            labeled_segment = labeled_segment - min(labeled_segment)
+            auto_convolve = scipy.signal.fftconvolve(labeled_segment, labeled_segment)
+            convolve_segment = scipy.signal.fftconvolve(labeled_segment, av_model)
+            convolve_list.append(max(auto_convolve))
+            convolve_list.append(max(convolve_trough)
+    return convolve_list
