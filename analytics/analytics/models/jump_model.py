a45b6c65bbceac6f1688c1c689e685c93ff9dccf
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-11-22 14:23:09 +0300

    refactoring jump model

--- analytics/analytics/models/jump_model.py
+++ analytics/analytics/models/jump_model.py
@@ -1,7 +1,6 @@
 from models import Model
 
 import utils
-from utils.segments import parse_segment
 import numpy as np
 import pandas as pd
 import scipy.signal
@@ -38,44 +37,17 @@ class JumpModel(Model):
         patterns_list = []
         for segment in segments:
             if segment['labeled']:
-                segment_from_index, segment_to_index, segment_data = parse_segment(segment, dataframe)
+                segment_from_index, segment_to_index, segment_data = utils.parse_segment(segment, dataframe)
                 percent_of_nans = segment_data.isnull().sum() / len(segment_data)
                 if percent_of_nans > 0 or len(segment_data) == 0:
                     continue    
-                segment_min = min(segment_data)
-                segment_max = max(segment_data)
-                confidences.append(0.20 * (segment_max - segment_min))
-                flat_segment = segment_data.rolling(window = 5).mean()
-                flat_segment_dropna = flat_segment.dropna()
-                min_jump = min(flat_segment_dropna)
-                max_jump = max(flat_segment_dropna)
-                pdf = gaussian_kde(flat_segment_dropna)
-                x = np.linspace(flat_segment_dropna.min() - 1, flat_segment_dropna.max() + 1, len(flat_segment_dropna))
-                y = pdf(x)
-                ax_list = list(zip(x, y))
-                ax_list = np.array(ax_list, np.float32)
-                antipeaks_kde = argrelextrema(np.array(ax_list), np.less)[0]
-                peaks_kde = argrelextrema(np.array(ax_list), np.greater)[0]
-                try:
-                    min_peak_index = peaks_kde[0]
-                    segment_min_line = ax_list[min_peak_index, 0]
-                    max_peak_index = peaks_kde[1]
-                    segment_max_line = ax_list[max_peak_index, 0]
-                    segment_median = ax_list[antipeaks_kde[0], 0]
-                except IndexError:
-                    segment_max_line = max_jump
-                    segment_min_line = min_jump
-                    segment_median = (max_jump - min_jump) / 2 + min_jump               
-                jump_height = 0.95 * (segment_max_line - segment_min_line)
+                confidence = utils.find_confidence(segment_data)
+                segment_cent_index, jump_height, jump_length = utils.find_parameters(segment_data)
                 jump_height_list.append(jump_height)
-                jump_length = utils.find_jump_length(segment_data, segment_min_line, segment_max_line)
                 jump_length_list.append(jump_length)
-                cen_ind = utils.intersection_segment(flat_segment.tolist(), segment_median) #finds all interseprions with median
-                jump_center = cen_ind[0]
-                segment_cent_index = jump_center - 5 + segment_from_index
                 self.ijumps.append(segment_cent_index)
-                labeled_jump = data[segment_cent_index - self.state['WINDOW_SIZE'] : segment_cent_index + self.state['WINDOW_SIZE'] + 1]
-                labeled_jump = labeled_jump - min(labeled_jump)
+                labeled_jump = utils.get_interval(data, segment_cent_index, self.state['WINDOW_SIZE'])
+                labeled_jump = utils.subtract_min_without_nan(labeled_jump)
                 patterns_list.append(labeled_jump)
                 
         self.model_jump = utils.get_av_model(patterns_list)
@@ -84,25 +56,12 @@ class JumpModel(Model):
         del_conv_list = []
         for segment in segments:
             if segment['deleted']:
-                segment_from_index = utils.timestamp_to_index(dataframe, pd.to_datetime(segment['from'], unit='ms'))
-                segment_to_index = utils.timestamp_to_index(dataframe, pd.to_datetime(segment['to'], unit='ms'))
-                segment_data = data[segment_from_index: segment_to_index + 1]
-                if len(segment_data) == 0:
+                segment_from_index, segment_to_index, segment_data = utils.parse_segment(segment, dataframe)
+                if percent_of_nans > 0 or len(segment_data) == 0:
                     continue
-                flat_segment = segment_data.rolling(window = 5).mean()
-                flat_segment_dropna = flat_segment.dropna()
-                pdf = gaussian_kde(flat_segment_dropna)
-                x = np.linspace(flat_segment_dropna.min() - 1, flat_segment_dropna.max() + 1, len(flat_segment_dropna))
-                y = pdf(x)
-                ax_list = list(zip(x, y))
-                ax_list = np.array(ax_list, np.float32)
-                antipeaks_kde = argrelextrema(np.array(ax_list), np.less)[0]
-                segment_median = ax_list[antipeaks_kde[0], 0]
-                cen_ind = utils.intersection_segment(flat_segment.tolist(), segment_median) #finds all interseprions with median
-                jump_center = cen_ind[0]
-                segment_cent_index = jump_center - 5 + segment_from_index
-                deleted_jump = data[segment_cent_index - self.state['WINDOW_SIZE'] : segment_cent_index + self.state['WINDOW_SIZE'] + 1]
-                deleted_jump = deleted_jump - min(labeled_jump)
+                segment_cent_index = utils.find_parameters(segment_data)[0]
+                deleted_jump = utils.get_interval(data, segment_cent_index, self.state['WINDOW_SIZE'])
+                deleted_jump = utils.subtract_min_without_nan(deleted_jump)
                 del_conv_jump = scipy.signal.fftconvolve(deleted_jump, self.model_jump)
                 del_conv_list.append(max(del_conv_jump)) 
 
@@ -163,7 +122,7 @@ class JumpModel(Model):
         delete_low_bound = self.state['conv_del_min'] * 0.98
         for segment in segments:
             if segment > self.state['WINDOW_SIZE'] and segment < (len(data) - self.state['WINDOW_SIZE']):
-                convol_data = data[segment - self.state['WINDOW_SIZE'] : segment + self.state['WINDOW_SIZE'] + 1]
+                convol_data = utils.get_interval(data, segment, self.state['WINDOW_SIZE'])
                 percent_of_nans = convol_data.isnull().sum() / len(convol_data)
                 if percent_of_nans > 0.5:
                     delete_list.append(segment)
