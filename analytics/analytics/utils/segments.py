3c478cb8a8c5252ef1f03513e5aae5ac0433ff5b
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-11-22 14:05:28 +0300

    add new metods in utils

--- analytics/analytics/utils/segments.py
+++ analytics/analytics/utils/segments.py
@@ -34,4 +34,37 @@ def get_interval(data, center, window_size):
     left_bound = center - window_size
     right_bound = center + window_size + 1
     return data[left_bound, right_bound]
+
+def find_parameters(segment_data):
+    flat_segment = segment_data.rolling(window = 5).mean()
+    flat_segment_dropna = flat_segment.dropna()
+    segment_median, segment_max_line, segment_min_line = utils.get_distribution_density(flat_segment_dropna)
+    jump_height = 0.95 * (segment_max_line - segment_min_line)
+    jump_length = utils.find_jump_length(segment_data, segment_min_line, segment_max_line)
+    cen_ind = utils.intersection_segment(flat_segment.tolist(), segment_median) #finds all interseprions with median
+    jump_center = cen_ind[0]
+    segment_cent_index = jump_center - 5 + segment_from_index
+    return segment_cent_index, jump_height, jump_length
+
+def get_distribution_density(segment):
+    min_jump = min(segment)
+    max_jump = max(segment)
+    pdf = gaussian_kde(segment)
+    x = np.linspace(segment.min() - 1, segment.max() + 1, len(segment))
+    y = pdf(x)
+    ax_list = list(zip(x, y))
+    ax_list = np.array(ax_list, np.float32)
+    antipeaks_kde = argrelextrema(np.array(ax_list), np.less)[0]
+    peaks_kde = argrelextrema(np.array(ax_list), np.greater)[0]
+    try:
+        min_peak_index = peaks_kde[0]
+        segment_min_line = ax_list[min_peak_index, 0]
+        max_peak_index = peaks_kde[1]
+        segment_max_line = ax_list[max_peak_index, 0]
+        segment_median = ax_list[antipeaks_kde[0], 0]
+    except IndexError:
+        segment_max_line = max_jump
+        segment_min_line = min_jump
+        segment_median = (max_jump - min_jump) / 2 + min_jump
+    return segment_median, segment_max_line, segment_min_line
                                  
