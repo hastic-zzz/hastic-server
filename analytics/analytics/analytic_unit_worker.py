8db1dcce6cb7c82f85cc0291991d088b198e725c
Alexey Velikiy <av@corpglory.com>
2018-11-25 17:52:43 +0300

    basic cancelation in analytics

--- analytics/analytics/analytic_unit_worker.py
+++ analytics/analytics/analytic_unit_worker.py
@@ -14,16 +14,22 @@ class AnalyticUnitWorker:
 
     def __init__(self, analytic_unit_id: str, detector: detectors.Detector, executor: Executor):
         self.analytic_unit_id = analytic_unit_id
-        self.detector = detector
-        self.executor: Executor = executor
+        self._detector = detector
+        self._executor: Executor = executor
+        self._training_feature: asyncio.Future = None
 
     async def do_learn(
         self, segments: list, data: pd.DataFrame, cache: Optional[AnalyticUnitCache]
     ) -> AnalyticUnitCache:
-        new_cache: AnalyticUnitCache = await asyncio.get_event_loop().run_in_executor(
-            self.executor, self.detector.train, data, segments, cache
+        self._learning_feature = asyncio.get_event_loop().run_in_executor(
+            self._executor, self._detector.train, data, segments, cache
         )
+        new_cache: AnalyticUnitCache = await self._learning_feature
         return new_cache
 
     async def do_predict(self, data: pd.DataFrame, cache: Optional[AnalyticUnitCache]) -> dict:
-        return self.detector.predict(data, cache)
+        return self._detector.predict(data, cache)
+    
+    def cancel(self):
+        if self._training_feature is not None:
+            self._training_feature.cancel()
