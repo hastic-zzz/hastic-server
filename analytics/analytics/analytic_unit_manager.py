99980bd404aebe77e389b5081c1c67aad98f7359
Alexey Velikiy <av@corpglory.com>
2018-11-25 11:48:46 +0300

    AnalyticUnitManager

--- analytics/analytics/analytic_unit_manager.py
+++ analytics/analytics/analytic_unit_manager.py
@@ -6,25 +6,16 @@ import logging, traceback
 import detectors
 from analytic_unit_worker import AnalyticUnitWorker
 
+
 logger = logging.getLogger('AnalyticUnitManager')
 
 AnalyticUnitId = str
-analytic_workers: Dict[AnalyticUnitId, AnalyticUnitWorker] = dict()
 
 
-def __get_detector_by_type(analytic_unit_type) -> detectors.Detector:
+def get_detector_by_type(analytic_unit_type) -> detectors.Detector:
     return detectors.PatternDetector(analytic_unit_type)
 
-def __ensure_worker(analytic_unit_id, analytic_unit_type) -> AnalyticUnitWorker:
-    if analytic_unit_id in analytic_workers:
-        # TODO: check that type is the same
-        return analytic_workers[analytic_unit_id]
-    detector = __get_detector_by_type(analytic_unit_type)
-    worker = AnalyticUnitWorker(analytic_unit_id, detector)
-    analytic_workers[analytic_unit_id] = worker
-    return worker
-
-def __prepare_data(data: list):
+def prepare_data(data: list):
     """
         Takes list
         - converts it into pd.DataFrame,
@@ -39,32 +30,45 @@ def __prepare_data(data: list):
 
     return data
 
-async def handle_analytic_task(task):
-    try:
-        payload = task['payload']
 
-        worker = __ensure_worker(task['analyticUnitId'], payload['pattern'])
+class AnalyticUnitManager:
 
-        data = __prepare_data(payload['data'])
-        result_payload = {}
-        if task['type'] == 'LEARN':
-            result_payload = await worker.do_learn(payload['segments'], data, payload['cache'])
-        elif task['type'] == 'PREDICT':
-            result_payload = await worker.do_predict(data, payload['cache'])
-        else:
-            raise ValueError('Unknown task type "%s"' % task['type'])
-        return {
-            'status': 'SUCCESS',
-            'payload': result_payload
-        }
+    def __init__(self):
+        self.analytic_workers: Dict[AnalyticUnitId, AnalyticUnitWorker] = dict()
 
-    except Exception as e:
-        error_text = traceback.format_exc()
-        logger.error("handle_analytic_task exception: '%s'" % error_text)
-        # TODO: move result to a class which renders to json for messaging to analytics
-        return {
-            'status': 'FAILED',
-            'error': str(e)
-        }
+    def __ensure_worker(self, analytic_unit_id, analytic_unit_type) -> AnalyticUnitWorker:
+        if analytic_unit_id in self.analytic_workers:
+            # TODO: check that type is the same
+            return self.analytic_workers[analytic_unit_id]
+        detector = get_detector_by_type(analytic_unit_type)
+        worker = AnalyticUnitWorker(analytic_unit_id, detector)
+        self.analytic_workers[analytic_unit_id] = worker
+        return worker
+
+    async def handle_analytic_task(self, task):
+        try:
+            payload = task['payload']
+            worker = self.__ensure_worker(task['analyticUnitId'], payload['pattern'])
+            data = prepare_data(payload['data'])
+            result_payload = {}
+            if task['type'] == 'LEARN':
+                result_payload = await worker.do_learn(payload['segments'], data, payload['cache'])
+            elif task['type'] == 'PREDICT':
+                result_payload = await worker.do_predict(data, payload['cache'])
+            else:
+                raise ValueError('Unknown task type "%s"' % task['type'])
+            return {
+                'status': 'SUCCESS',
+                'payload': result_payload
+            }
+
+        except Exception as e:
+            error_text = traceback.format_exc()
+            logger.error("handle_analytic_task exception: '%s'" % error_text)
+            # TODO: move result to a class which renders to json for messaging to analytics
+            return {
+                'status': 'FAILED',
+                'error': str(e)
+            }
 
 
