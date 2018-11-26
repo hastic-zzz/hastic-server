3e31c28d557e5da1608341cad80b35f4079ef84b
Alexey Velikiy <av@corpglory.com>
2018-11-26 03:33:25 +0300

    basic cancelation in analytics

--- analytics/analytics/analytic_unit_worker.py
+++ analytics/analytics/analytic_unit_worker.py
@@ -4,7 +4,7 @@ import logging
 import pandas as pd
 from typing import Optional
 from models import AnalyticUnitCache
-from concurrent.futures import Executor
+from concurrent.futures import Executor, CancelledError
 import asyncio
 
 logger = logging.getLogger('AnalyticUnitWorker')
@@ -18,14 +18,18 @@ class AnalyticUnitWorker:
         self._executor: Executor = executor
         self._training_feature: asyncio.Future = None
 
-    async def do_learn(
+    async def do_train(
         self, segments: list, data: pd.DataFrame, cache: Optional[AnalyticUnitCache]
     ) -> AnalyticUnitCache:
-        self._learning_feature = asyncio.get_event_loop().run_in_executor(
+        self._training_feature = asyncio.get_event_loop().run_in_executor(
             self._executor, self._detector.train, data, segments, cache
         )
-        new_cache: AnalyticUnitCache = await self._learning_feature
-        return new_cache
+        try:
+            new_cache: AnalyticUnitCache = await self._training_feature
+            return new_cache
+        except CancelledError as e:
+            return cache
+        
 
     async def do_predict(self, data: pd.DataFrame, cache: Optional[AnalyticUnitCache]) -> dict:
         return self._detector.predict(data, cache)
