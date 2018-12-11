e4ba3b172e4608570221135905864a359a13129f
VargBurz <voidstone1@mail.ru>
2018-12-12 01:08:24 +0300

    fix methods

--- analytics/tests/test_utils.py
+++ analytics/tests/test_utils.py
@@ -95,39 +95,40 @@ class TestUtils(unittest.TestCase):
         segment = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
         segment = pd.Series(segment)
         jump_center = [10, 11]
-        self.assertIn(utils.find_jump_parameters(segment, 0)[0], jump_center)
+        self.assertIn(utils.find_parameters(segment, 0, 'jump')[0], jump_center)
     
     def test_find_jump_parameters_height(self):
         segment = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
         segment = pd.Series(segment)
         jump_height = [3.5, 4]
-        self.assertGreaterEqual(utils.find_jump_parameters(segment, 0)[1], jump_height[0])
-        self.assertLessEqual(utils.find_jump_parameters(segment, 0)[1], jump_height[1])
+        self.assertGreaterEqual(utils.find_parameters(segment, 0, 'jump')[1], jump_height[0])
+        self.assertLessEqual(utils.find_parameters(segment, 0, 'jump')[1], jump_height[1])
     
     def test_find_jump_parameters_length(self):
         segment = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
         segment = pd.Series(segment)
         jump_length = 2
-        self.assertEqual(utils.find_jump_parameters(segment, 0)[2], jump_length)
+        self.assertEqual(utils.find_parameters(segment, 0, 'jump')[2], jump_length)
     
     def test_find_drop_parameters_center(self):
         segment = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         segment = pd.Series(segment)
-        drop_center = [14, 15]
-        self.assertIn(utils.find_drop_parameters(segment, 0)[0], drop_center)
+        drop_center = [14, 15, 16]
+        self.assertIn(utils.find_parameters(segment, 0, 'drop')[0], drop_center)
     
     def test_find_drop_parameters_height(self):
         segment = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         segment = pd.Series(segment)
         drop_height = [3.5, 4]
-        self.assertGreaterEqual(utils.find_drop_parameters(segment, 0)[1], drop_height[0])
-        self.assertLessEqual(utils.find_drop_parameters(segment, 0)[1], drop_height[1])
+        self.assertGreaterEqual(utils.find_parameters(segment, 0, 'drop')[1], drop_height[0])
+        self.assertLessEqual(utils.find_parameters(segment, 0, 'drop')[1], drop_height[1])
     
     def test_find_drop_parameters_length(self):
         segment = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         segment = pd.Series(segment)
         drop_length = 2
-        self.assertEqual(utils.find_drop_parameters(segment, 0)[2], drop_length)
+        self.assertEqual(utils.find_parameters(
+            segment, 0, 'drop')[2], drop_length)
 
 if __name__ == '__main__':
     unittest.main()
