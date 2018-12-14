6acc6d32e8640326de5faaef8762349f34aa2ee5
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2018-12-14 15:53:56 +0300

    fix jump test

--- analytics/tests/test_dataset.py
+++ analytics/tests/test_dataset.py
@@ -41,13 +41,13 @@ class TestDataset(unittest.TestCase):
             self.fail('Model {} raised unexpectedly'.format(model_name))
         
     def test_jump_antisegments(self):
-        data_val = [1.0, 1.0, 1.0, 2.0, 3.0, 2.0, 1.0, 1.0, 1.0, 1.0, 5.0, 7.0, 5.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+        data_val = [1.0, 1.0, 1.0, 2.0, 2.0, 5.0, 5.0, 5.0, 2.0, 2.0, 2.0, 2.0, 9.0, 9.0, 9.0, 1.0, 1.0, 1.0, 1.0, 1.0]
         data_ind = [1523889000000 + i for i in range(20)]
         data = {'timestamp': data_ind, 'value': data_val}
         dataframe = pd.DataFrame(data)
         dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], unit='ms')
-        segments = [{'_id': 'Esl7uetLhx4lCqHa', 'analyticUnitId': 'opnICRJwOmwBELK8', 'from': 1523889000010, 'to': 1523889000012, 'labeled': True, 'deleted': False},
-                    {'_id': 'Esl7uetLhx4lCqHa', 'analyticUnitId': 'opnICRJwOmwBELK8', 'from': 1523889000003, 'to': 1523889000005, 'labeled': False, 'deleted': True}]
+        segments = [{'_id': 'Esl7uetLhx4lCqHa', 'analyticUnitId': 'opnICRJwOmwBELK8', 'from': 1523889000011, 'to': 1523889000015, 'labeled': True, 'deleted': False},
+                    {'_id': 'Esl7uetLhx4lCqHa', 'analyticUnitId': 'opnICRJwOmwBELK8', 'from': 1523889000003, 'to': 1523889000007, 'labeled': False, 'deleted': True}]
 
         try:
             model = models.JumpModel()
