a26341d0339730b53e42f20bba290fb8ede5cfab
VargBurz <voidstone1@mail.ru>
2018-12-14 08:01:04 +0300

    test

--- analytics/tests/test_dataset.py
+++ analytics/tests/test_dataset.py
@@ -23,6 +23,26 @@ class TestDataset(unittest.TestCase):
                 model.fit(dataframe, segments, dict())
         except ValueError:
             self.fail('Model {} raised unexpectedly'.format(model_name))
+    
+    def test_antisegments(self):
+        data = [[1523889000000 + i, float(1.0)] for i in range(20)]
+        dataframe = pd.DataFrame(data, columns=['timestamp', 'value'])
+        segments = [{'_id': 'Esl7uetLhx4lCqHa', 'analyticUnitId': 'opnICRJwOmwBELK8', 'from': 1523889000001, 'to': 1523889000004, 'labeled': True, 'deleted': False},
+                    {'_id': 'Esl7uetLhx4lCqHa', 'analyticUnitId': 'opnICRJwOmwBELK8', 'from': 1523889000009, 'to': 1523889000012, 'labeled': False, 'deleted': True}]
+
+        model_instances = [
+            models.JumpModel(),
+            models.DropModel(),
+            models.GeneralModel(),
+            models.PeakModel(),
+            models.TroughModel()
+        ]
+        try:
+            for model in model_instances:
+                model_name = model.__class__.__name__
+                model.fit(dataframe, segments, dict())
+        except ValueError:
+            self.fail('Model {} raised unexpectedly'.format(model_name))
 
 if __name__ == '__main__':
     unittest.main()
