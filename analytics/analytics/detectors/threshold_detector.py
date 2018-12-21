4ce1a993d959412d7d39d7287fafbed1b3a81b9d
rozetko <rozetko@corpglory.com>
2018-12-21 20:52:49 +0300

    Implement detector

--- analytics/analytics/detectors/threshold_detector.py
+++ analytics/analytics/detectors/threshold_detector.py
@@ -16,10 +16,45 @@ class ThresholdDetector(Detector):
         pass
 
     def train(self, dataframe: pd.DataFrame, threshold: dict, cache: Optional[ModelCache]) -> ModelCache:
-        pass
+        return {
+            'cache': {
+                'value': threshold['value'],
+                'condition': threshold['condition']
+            }
+        }
 
     def detect(self, dataframe: pd.DataFrame, cache: Optional[ModelCache]) -> dict:
-        pass
+        value = cache['value']
+        condition = cache['condition']
+
+        last_entry = dataframe.iloc[-1]
+        last_value = last_entry['value']
+        # TODO: convert from nanoseconds to millisecond in a better way: not by dividing by 10^6
+        last_time = last_entry['timestamp'].value / 1000000
+
+        segment = (last_time, last_time)
+        segments = []
+        if condition == '>':
+            if last_value > value:
+                segments.append(segment)
+        elif condition == '>=':
+            if last_value >= value:
+                segments.append(segment)
+        elif condition == '=':
+            if last_value == value:
+                segments.append(segment)
+        elif condition == '<=':
+            if last_value <= value:
+                segments.append(segment)
+        elif condition == '<':
+            if last_value < value:
+                segments.append(segment)
+
+        return {
+            'cache': cache,
+            'segments': segments,
+            'lastDetectionTime': last_time
+        }
 
     def recieve_data(self, data: pd.DataFrame, cache: Optional[ModelCache]) -> Optional[dict]:
-        pass
+        return self.detect(self.bucket.data, cache)
