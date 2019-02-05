b09e626657b25b362ab1366ff7404fcfd5365f1a
VargBurz <voidstone1@mail.ru>
2019-02-05 23:52:48 +0300

    fix general

--- analytics/analytics/models/general_model.py
+++ analytics/analytics/models/general_model.py
@@ -63,8 +63,8 @@ class GeneralModel(Model):
         data = utils.cut_dataframe(dataframe)
         data = data['value']
         pat_data = self.model_gen
-        if len(pat_data.nonzero()[0]) == 0:
-            raise NameError('Labeled patterns must not be empty')
+        if pat_data.count(0) == len(pat_data):
+            raise ValueError('Labeled patterns must not be empty')
 
         for i in range(self.state['WINDOW_SIZE'] * 2, len(data)):
             watch_data = data[i - self.state['WINDOW_SIZE'] * 2: i]
