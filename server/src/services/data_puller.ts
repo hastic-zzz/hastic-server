689b901d560395aa1323e07b353fe2112f9b05d8
amper43 <amper43@corpglory.com>
2019-02-21 21:36:35 +0300

    remove dot

--- server/src/services/data_puller.ts
+++ server/src/services/data_puller.ts
@@ -41,7 +41,7 @@ export class DataPuller {
 
   private _grafanaConnectionRefusedReporter = this._availableReporter(
     null,
-    `data puller: can't connect to Grafana. Check GRAFANA_URL.`
+    `data puller: can't connect to Grafana. Check GRAFANA_URL`
   );
 
   private _unitTimes: { [analyticUnitId: string]: number } = {};
