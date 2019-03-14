8c180c2fc8e09817e1871bd3f616a2c9dc65c606
Evgeny Smyshlyaev <amper43@gmail.com>
2019-03-14 13:39:03 +0300

    Revert "Futures for detect and recieve #264 (#433)"
    This reverts commit 006adbfeb3fc13a4a894acae3ca9388e3c95c9f0.

--- analytics/analytics/analytic_unit_worker.py
+++ analytics/analytics/analytic_unit_worker.py
@@ -17,8 +17,6 @@ class AnalyticUnitWorker:
         self._detector = detector
         self._executor: Executor = executor
         self._training_feature: asyncio.Future = None
-        self._detection_feature: asyncio.Future = None
-        self._recieve_feature: asyncio.Future = None
 
     async def do_train(
         self, payload: Union[list, dict], data: pd.DataFrame, cache: Optional[ModelCache]
@@ -33,30 +31,11 @@ class AnalyticUnitWorker:
             return cache
 
     async def do_detect(self, data: pd.DataFrame, cache: Optional[ModelCache]) -> dict:
-        self._detection_feature = asyncio.get_event_loop().run_in_executor(
-            self._executor, self._detector.detect, data, cache
-        )
-        try:
-            detect_results = await self._detection_feature
-            return detect_results
-        except CancelledError as e:
-            return {}
-
+        return self._detector.detect(data, cache)
 
     def cancel(self):
         if self._training_feature is not None:
             self._training_feature.cancel()
-        if self._detection_feature is not None:
-            self._detection_feature.cancel()
-        if self._recieve_feature is not None:
-            self._recieve_feature.cancel()
 
     async def recieve_data(self, data: pd.DataFrame, cache: Optional[ModelCache]):
-        self._recieve_feature = asyncio.get_event_loop().run_in_executor(
-            self._executor, self._detector.recieve_data, data, cache
-        )
-        try:
-            detect_results = await self._recieve_feature
-            return detect_results
-        except CancelledError as e:
-            return None
+        return self._detector.recieve_data(data, cache)
