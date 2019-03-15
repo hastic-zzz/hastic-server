978a42c2b896783597f1e98e41782113a4202c21
amper43 <amper43@corpglory.com>
2019-03-15 15:42:57 +0300

    add 100 detects

--- analytics/tools/send_zmq_message.py
+++ analytics/tools/send_zmq_message.py
@@ -13,8 +13,16 @@ async def handle_loop():
 
         print(text)
 
+async def send_detect():
+    f = open('push', 'rb')
+    data = f.read()
+    await socket.send(data)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     socket.send(b'PING')
-    loop.run_until_complete(handle_loop())
+    detects = [send_detect() for i in range(100)]
+    detects_group = asyncio.gather(*detects)
+    handle_group = asyncio.gather(handle_loop())
+    common_group = asyncio.gather(handle_group, detects_group)
+    loop.run_until_complete(common_group)
