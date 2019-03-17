29b1977a39f63ec4f7c65b1915ce7a5852a128ba
Alexey Velikiy <gmpota@gmail.com>
2019-03-17 04:59:54 +0100

    Common mechanism for handling errors #474 (#475)
    * handling middleware
    
    * rm try/catch in routers

--- server/src/routes/panel_router.ts
+++ server/src/routes/panel_router.ts
@@ -5,65 +5,35 @@ import * as Router from 'koa-router';
 
 
 async function getAnalyticUnits(ctx: Router.IRouterContext) {
-  try {
-    let panelUrl: string = ctx.request.query.panelUrl;
-    if(panelUrl === undefined || panelUrl === '') {
-      throw new Error('panelUrl is missing');
-    }
-
-    const analyticUnits = await Panel.findOne({ panelUrl });
-
-    ctx.response.body = { analyticUnits };
-  } catch(e) {
-    ctx.response.status = 500;
-    ctx.response.body = {
-      code: 500,
-      message: `GET /panel error: ${e.message}`
-    };
+  let panelUrl: string = ctx.request.query.panelUrl;
+  if(panelUrl === undefined || panelUrl === '') {
+    throw new Error('panelUrl is missing');
   }
+  const analyticUnits = await Panel.findOne({ panelUrl });
+  ctx.response.body = { analyticUnits };
 }
 
 async function addAnalyticUnit(ctx: Router.IRouterContext) {
-  try {
-    let { panelUrl, analyticUnitId } = ctx.request.body as {
-      panelUrl: string, analyticUnitId: AnalyticUnitId
-    };
-
-    await Panel.insertAnalyticUnit(panelUrl, analyticUnitId);
-
-    ctx.response.body = {
-      code: 200,
-      message: 'Success'
-    };
-  } catch(e) {
-    ctx.response.status = 500;
-    ctx.response.body = {
-      code: 500,
-      message: `POST /panel error: ${e.message}`
-    };
-  }
+  let { panelUrl, analyticUnitId } = ctx.request.body as {
+    panelUrl: string, analyticUnitId: AnalyticUnitId
+  };
+  await Panel.insertAnalyticUnit(panelUrl, analyticUnitId);
+  ctx.response.body = {
+    code: 200,
+    message: 'Success'
+  };
 }
 
 async function deleteAnalyticUnit(ctx: Router.IRouterContext) {
-  try {
-    let { panelUrl, analyticUnitId } = ctx.request.body as {
-      panelUrl: string, analyticUnitId: AnalyticUnitId
-    };
-
-    // TODO: stop task when analytic unit is removed
-    await Panel.removeAnalyticUnit(panelUrl, analyticUnitId);
-
-    ctx.response.body = {
-      code: 200,
-      message: 'Success'
-    };
-  } catch(e) {
-    ctx.response.status = 500;
-    ctx.response.body = {
-      code: 500,
-      message: `DELETE /panel error: ${e.message}`
-    };
-  }
+  let { panelUrl, analyticUnitId } = ctx.request.body as {
+    panelUrl: string, analyticUnitId: AnalyticUnitId
+  };
+  // TODO: stop task when analytic unit is removed
+  await Panel.removeAnalyticUnit(panelUrl, analyticUnitId);
+  ctx.response.body = {
+    code: 200,
+    message: 'Success'
+  };
 }
 
 export const router = new Router();
--- server/src/routes/segments_router.ts
+++ server/src/routes/segments_router.ts
@@ -7,63 +7,41 @@ import * as Router from 'koa-router';
 
 
 async function getSegments(ctx: Router.IRouterContext) {
-  try {
-    let id: AnalyticUnitId = ctx.request.query.id;
-    if(id === undefined || id === '') {
-      throw new Error('analyticUnitId (id) is missing');
-    }
-    let query: Segment.FindManyQuery = {};
-
-    if(!isNaN(+ctx.request.query.lastSegmentId)) {
-      query.intexGT = +ctx.request.query.lastSegmentId;
-    }
-    if(!isNaN(+ctx.request.query.from)) {
-      query.timeFromGTE = +ctx.request.query.from;
-    }
-    if(!isNaN(+ctx.request.query.to)) {
-      query.timeToLTE = +ctx.request.query.to;
-    }
-
-    let segments = await Segment.findMany(id, query);
-
-    ctx.response.body = { segments };
-  } catch(e) {
-    console.error(e);
-    ctx.response.status = 500;
-    ctx.response.body = {
-      code: 500,
-      message: `GET /segments error: ${e.message}`
-    };
+  let id: AnalyticUnitId = ctx.request.query.id;
+  if(id === undefined || id === '') {
+    throw new Error('analyticUnitId (id) is missing');
   }
+  let query: Segment.FindManyQuery = {};
+
+  if(!isNaN(+ctx.request.query.lastSegmentId)) {
+    query.intexGT = +ctx.request.query.lastSegmentId;
+  }
+  if(!isNaN(+ctx.request.query.from)) {
+    query.timeFromGTE = +ctx.request.query.from;
+  }
+  if(!isNaN(+ctx.request.query.to)) {
+    query.timeToLTE = +ctx.request.query.to;
+  }
+  let segments = await Segment.findMany(id, query);
+  ctx.response.body = { segments };
 }
 
 async function updateSegments(ctx: Router.IRouterContext) {
-  try {
+  const {
+    addedSegments, id, removedSegments: removedIds
+  } = ctx.request.body as {
+    addedSegments: any[], id: AnalyticUnitId, removedSegments: Segment.SegmentId[]
+  };
 
-    const {
-      addedSegments, id, removedSegments: removedIds
-    } = ctx.request.body as {
-      addedSegments: any[], id: AnalyticUnitId, removedSegments: Segment.SegmentId[]
-    };
+  const segmentsToInsert: Segment.Segment[] = addedSegments.map(
+    s => Segment.Segment.fromObject({ analyticUnitId: id, ...s })
+  );
 
-    const segmentsToInsert: Segment.Segment[] = addedSegments.map(
-      s => Segment.Segment.fromObject({ analyticUnitId: id, ...s })
-    );
+  const { addedIds } = await AnalyticsController.updateSegments(
+    id, segmentsToInsert, removedIds
+  );
 
-    const { addedIds } = await AnalyticsController.updateSegments(
-      id, segmentsToInsert, removedIds
-    );
-
-    ctx.response.body = { addedIds };
-    
-  } catch(e) {
-    console.error(e);
-    ctx.response.status = 500;
-    ctx.response.body = {
-      code: 500,
-      message: `PATCH /segments (learning) error: ${e.message}`
-    };
-  }
+  ctx.response.body = { addedIds }; 
 }
 
 export const router = new Router();
