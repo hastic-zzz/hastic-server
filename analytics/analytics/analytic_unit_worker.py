3bf51dbc2d6008cd9a317fd0c1acb0a4b7340da8
rozetko <rozetko@corpglory.com>
2019-03-25 20:57:15 +0300

    Add learning timeout

--- analytics/analytics/analytic_unit_worker.py
+++ analytics/analytics/analytic_unit_worker.py
@@ -4,7 +4,7 @@ import logging
 import pandas as pd
 from typing import Optional, Union
 from models import ModelCache
-from concurrent.futures import Executor, CancelledError
+from concurrent.futures import Executor, CancelledError, TimeoutError
 import asyncio
 
 logger = logging.getLogger('AnalyticUnitWorker')
@@ -21,14 +21,17 @@ class AnalyticUnitWorker:
     async def do_train(
         self, payload: Union[list, dict], data: pd.DataFrame, cache: Optional[ModelCache]
     ) -> ModelCache:
-        self._training_feature = asyncio.get_event_loop().run_in_executor(
-            self._executor, self._detector.train, data, payload, cache
+        self._training_feature = self._executor.submit(
+            self._detector.train, data, payload, cache
         )
         try:
-            new_cache: ModelCache = await self._training_feature
+            # TODO: configurable timeout
+            new_cache: ModelCache = self._training_feature.result(timeout = 120)
             return new_cache
         except CancelledError as e:
             return cache
+        except TimeoutError:
+            raise 'Timeout ({}s) exceeded while learning'
 
     async def do_detect(self, data: pd.DataFrame, cache: Optional[ModelCache]) -> dict:
         return self._detector.detect(data, cache)
