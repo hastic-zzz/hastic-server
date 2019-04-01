56fcf2cb8709eef034eb8b7f492fc10099dab927
amper43 <amper43@corpglory.com>
2019-04-01 17:11:19 +0300

    add test file

---
+++ analytics/tests/test_detector_chunks.py
@@ -0,0 +1,10 @@
+import unittest
+import detectors.pattern_detector
+
+class TestUtils(unittest.TestCase):
+
+    def test_chunks_generator(self):
+        pass
+
+    def test_detector(self):
+        pass
