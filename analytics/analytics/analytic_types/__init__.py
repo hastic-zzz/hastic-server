194a8b9de6a4c34ce1ebe3bc5725d471bfa0b016
Alexey Velikiy <gmpota@gmail.com>
2019-04-15 19:42:18 +0300

    Analytic types (#573)

---
+++ analytics/analytics/analytic_types/__init__.py
@@ -0,0 +1,14 @@
+"""
+It is the place where we put all classes and types
+which is common for all of the code.
+
+For example, if you write someting which is used
+in analytic_unit_manager, it should be here.
+
+If you create something spicific which is used only in one place, 
+like PatternDetectionCache, then it should not be here.
+"""
+
+from analytic_types.data_bucket import DataBucket
+
+AnalyticUnitId = str
--- analytics/analytics/buckets/__init__.py
+++
@@ -1,1 +0,0 @@
-from buckets.data_bucket import DataBucket
