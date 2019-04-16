1590003dd0296e597148d1486d1fd0be8abc5e6e
VargBurz <voidstone1@mail.ru>
2019-04-16 17:46:09 +0300

    add import

--- analytics/analytics/models/general_model.py
+++ analytics/analytics/models/general_model.py
@@ -7,6 +7,7 @@ import scipy.signal
 from scipy.fftpack import fft
 from scipy.signal import argrelextrema
 from scipy.stats.stats import pearsonr
+from typing import Optional
 import math
 from scipy.stats import gaussian_kde
 from scipy.stats import norm
