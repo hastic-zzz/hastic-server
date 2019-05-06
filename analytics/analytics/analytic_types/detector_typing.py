fbe9eadccacf4c0e1d237d5f6e11fa9235ab7a29
amper43 <amper43@corpglory.com>
2019-05-06 20:45:30 +0300

    fix

--- analytics/analytics/analytic_types/detector_typing.py
+++ analytics/analytics/analytic_types/detector_typing.py
@@ -15,18 +15,3 @@ class DetectionResult:
         self.cache = cache
         self.segments = segments
         self.lastDetectionTime = lastDetectionTime
-
-@utils.meta.JSONClass
-class DetectorCache:
-    pass
-
-@utils.meta.JSONClass
-class ThresholdCache(DetectorCache):
-
-    def __init__(
-        self,
-        value: float = 0.0,
-        condition: str = '>'
-    ):
-        self.value = value
-        self.condition = condition
