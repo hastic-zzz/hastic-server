7dd7d7f8931a1236c67cccffde3a1986c8e49cf5
rozetko <rozetko@corpglory.com>
2019-05-08 15:29:50 +0300

    Remove params from segment

--- analytics/analytics/analytic_types/segment.py
+++ analytics/analytics/analytic_types/segment.py
@@ -5,17 +5,14 @@ class Segment:
     Used for segment manipulation instead of { 'from': ..., 'to': ... } dict
     '''
 
-    def __init__(self, from_timestamp: int, to_timestamp: int, params: Optional[dict] = None):
+    def __init__(self, from_timestamp: int, to_timestamp: int):
         if to_timestamp < from_timestamp:
             raise ValueError(f'Can`t create segment with to < from: {to_timestamp} < {from_timestamp}')
         self.from_timestamp = from_timestamp
         self.to_timestamp = to_timestamp
-        self.params = params
 
     def to_json(self):
         return {
             'from': self.from_timestamp,
-            'to': self.to_timestamp,
-            # TODO: remove params
-            'params': self.params
+            'to': self.to_timestamp
         }
