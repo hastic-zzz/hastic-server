0fd00d11e457bb62b229a5e190fe436f391e40a9
rozetko <rozetko@corpglory.com>
2019-05-08 14:25:27 +0300

    Minor fixes

--- analytics/analytics/analytic_types/segment.py
+++ analytics/analytics/analytic_types/segment.py
@@ -1,8 +1,16 @@
 from typing import Optional
 
 class Segment:
+    '''
+    Used as Detector result
+    Stores fields that node expects: 
+    - `from`, `to` timestamps
+    - `params`
+    '''
 
     def __init__(self, start_timestamp: int, end_timestamp: int, params: Optional[dict] = None):
+        if end_timestamp < start_timestamp:
+            raise ValueError(f'Can`t create segment with to < from: {end_timestamp} < {start_timestamp}')
         self.start_timestamp = start_timestamp
         self.end_timestamp = end_timestamp
         self.params = params
@@ -10,5 +18,6 @@ class Segment:
     def to_json(self):
         return {
             'from': self.start_timestamp,
-            'to': self.end_timestamp
+            'to': self.end_timestamp,
+            'params': self.params
         }
