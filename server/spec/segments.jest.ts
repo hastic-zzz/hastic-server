3a2da9f3e9105962ef7d4bf99e37c6f360c0b7e9
rozetko <rozetko@hastic.io>
2019-08-12 21:31:30 +0300

    Apply suggestions from code review

--- server/spec/segments.jest.ts
+++ server/spec/segments.jest.ts
@@ -32,19 +32,19 @@ afterEach(async () => {
   clearDB();
 });
 
-describe('Check deleted segments', function () {
+describe('Check deleted segments', function() {
   let payload = {
     lastDetectionTime: 0,
     segments: [],
     cache: null
   };
 
-  it('previous segments not found', async function () {
+  it('previous segments not found', async function() {
     payload.segments = segmentBuilder([[0, 1], [4, 5]]);
     expect(await getDeletedSegments(TEST_ID, payload)).toEqual(segmentBuilder([[2, 3]]));
   });
 
-  it('all previous segments found', async function () {
+  it('all previous segments found', async function() {
     payload.segments = segmentBuilder([[0, 1], [2, 3], [4, 5]]);
     expect(await getDeletedSegments(TEST_ID, payload)).toEqual([]);
   });
