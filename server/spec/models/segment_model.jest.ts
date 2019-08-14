b0eb06a018827e2c3768f3f77c080d4185a6b9a3
rozetko <rozetko@corpglory.com>
2019-08-14 16:01:14 +0300

    fix import paths

--- server/spec/models/segment_model.jest.ts
+++ server/spec/models/segment_model.jest.ts
@@ -1,6 +1,6 @@
-import { buildSegments, clearDB, TEST_ANALYTIC_UNIT_ID } from './utils_for_tests/segments';
+import { buildSegments, clearDB, TEST_ANALYTIC_UNIT_ID } from '../utils_for_tests/segments';
 
-import * as Segment from '../src/models/segment_model';
+import * as Segment from '../../src/models/segment_model';
 
 import * as _ from 'lodash';
 
