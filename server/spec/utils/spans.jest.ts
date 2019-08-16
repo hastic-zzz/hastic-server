c6b524ac08a722d0a87e337305ad5ca6ba6daba2
rozetko <rozetko@corpglory.com>
2019-08-16 20:31:30 +0300

    fix types

--- server/spec/utils/spans.jest.ts
+++ server/spec/utils/spans.jest.ts
@@ -3,11 +3,11 @@ import { cutSpanWithSpans } from '../../src/utils/spans';
 import 'jest';
 
 
-function cutSpan(from: number, to: number, cuts: [number, number][]): number[][] {
+function cutSpan(from: number, to: number, cuts: [number, number][]): [number, number][] {
   return cutSpanWithSpans(
     { from: from, to: to },
     cuts.map(([from, to]) => ({ from, to }))
-  ).map(({ from, to }) => [from, to]);
+  ).map<[number, number]>(({ from, to }) => [from, to]);
 }
 
 describe('cutSpanWithSpans', function() {
