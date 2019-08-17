66f853408fbe54af5767101ff46c373c75b320ac
Alexey Velikiy <av@corpglory.com>
2019-08-17 18:40:51 +0300

    add infinite spans

--- server/spec/utils/spans.jest.ts
+++ server/spec/utils/spans.jest.ts
@@ -32,12 +32,18 @@ describe('cutSpanWithSpans', function() {
     expect(cutSpan(4, 10, [])).toEqual([[4, 10]]);
   });
 
-  it('should handle empty one-point cuts', function() {
+  it('should handle one-point cuts', function() {
     expect(cutSpan(1, 10, [[5, 5]])).toEqual([[1, 5], [5, 10]]);
     expect(cutSpan(1, 10, [[1, 1]])).toEqual([[1, 10]]);
     expect(cutSpan(1, 10, [[10, 10]])).toEqual([[1, 10]]);
     expect(cutSpan(1, 10, [[11, 11]])).toEqual([[1, 10]]);
     expect(cutSpan(1, 10, [[0.9, 0.0]])).toEqual([[1, 10]]);
+  });
+
+  it('should handle when spans are infitie', function() {
+    expect(cutSpan(0, Infinity, [[5, 10]])).toEqual([[0, 5], [10, Infinity]]);
+    expect(cutSpan(0, 6, [[0, Infinity]])).toEqual([]);
+    expect(cutSpan(-Infinity, Infinity, [[-Infinity, Infinity]])).toEqual([]);
   });
 
   it('should handle case when from and to are inside of one big span', function() {
