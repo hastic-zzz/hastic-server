71be592203bda0fd2c1f783dd0ba2f9f4589ff30
amper43 <amper43@corpglory.com>
2019-09-06 14:36:15 +0300

    fix

--- server/src/services/data_layer/mongodb.ts
+++ server/src/services/data_layer/mongodb.ts
@@ -2,17 +2,18 @@ import { Collection, FilterQuery, ObjectID } from 'mongodb';
 import { wrapIdToQuery, wrapIdsToQuery, isEmptyArray } from './utils';
 
 import * as _ from 'lodash';
-import { strict } from 'assert';
 
 
 export class MongoDbAdapter {
 
   async dbInsertOne(nd: Collection, doc: object): Promise<string> {
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#insertOne
     const newDoc = await nd.insertOne(doc);
     return newDoc.insertedId.toString();
   }
   
   async dbInsertMany(nd: Collection, docs: object[]): Promise<string[]> {
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#insertMany
     if(docs.length === 0) {
       return Promise.resolve([]);
     }
@@ -20,20 +21,18 @@ export class MongoDbAdapter {
     return _.map(newDocs.insertedIds, (k,v) => v.toString());
   }
   
-  async dbUpdateOne(nd: Collection, query: FilterQuery<string | object>, updateQuery: object): Promise<any> {
-    // https://github.com/louischatriot/nedb#updating-documents
+  async dbUpdateOne(nd: Collection, query: FilterQuery<string | object>, updateQuery: object): Promise<void> {
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#updateOne
     let mongodbUpdateQuery = { $set: updateQuery }
     query = wrapIdToQuery(query);
     const affected = await nd.updateOne(
       query,
-      mongodbUpdateQuery,
-  //TODO: check follow option: { returnUpdatedDocs: true },
+      mongodbUpdateQuery
     );
-    return affected.result; // return updated object
   }
   
   async dbUpdateMany(collection: Collection, query: string[] | object, updateQuery: object): Promise<void> {
-      // https://github.com/louischatriot/nedb#updating-documents
+      // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#updateMany
       if(isEmptyArray(query)) {
       return Promise.resolve();
       }
@@ -42,13 +41,11 @@ export class MongoDbAdapter {
       const affectedDocuments = await collection.updateMany(
           query,
           mongodbUpdateQuery
-          //add returnUpdatedDocs: true, 
       );
-      //?? return updated object
   }
   
   async dbFindOne(collection: Collection, query: FilterQuery<string | object>): Promise<any> {
-    console.log(query);
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#findOne
     if(typeof query === 'string') {
       query = new ObjectID(query as string);
     }
@@ -58,32 +55,34 @@ export class MongoDbAdapter {
       doc._id = doc._id.toString();
     }
     return doc;
-
   }
   
   async dbFindMany(collection: Collection, query: string[] | object, sortQuery: object = {}): Promise<any[]> {
-      if(isEmptyArray(query)) {
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#find  
+    if(isEmptyArray(query)) {
       return Promise.resolve([]);
-      }
-      query = wrapIdsToQuery(query);
-      return await collection.find(query).sort(sortQuery).toArray();
+    }
+    query = wrapIdsToQuery(query);
+    return await collection.find(query).sort(sortQuery).toArray();
   }
   
   async dbRemoveOne(collection: Collection, query: FilterQuery<string | object>): Promise<boolean> {
-      query = wrapIdToQuery(query);
-      const deleted = await collection.deleteOne(query);
-      if(deleted.deletedCount > 1) {
-        throw new Error(`Removed ${deleted.deletedCount} elements with query: ${JSON.stringify(query)}. Only one is Ok.`);
-      }
-      return deleted.deletedCount == 1;
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#deleteOne
+    query = wrapIdToQuery(query);
+    const deleted = await collection.deleteOne(query);
+    if(deleted.deletedCount > 1) {
+      throw new Error(`Removed ${deleted.deletedCount} elements with query: ${JSON.stringify(query)}. Only one is Ok.`);
+    }
+    return deleted.deletedCount == 1;
   }
   
   async dbRemoveMany(collection: Collection, query: string[] | object): Promise<number> {
-      if(isEmptyArray(query)) {
+    // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#deleteMany
+    if(isEmptyArray(query)) {
       return Promise.resolve(0);
-      }
-      query = wrapIdsToQuery(query);
-      const deleted = await collection.deleteMany(query);
-      return deleted.deletedCount;
+    }
+    query = wrapIdsToQuery(query);
+    const deleted = await collection.deleteMany(query);
+    return deleted.deletedCount;
   }
 }
