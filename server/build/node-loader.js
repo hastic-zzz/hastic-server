3126cac1940504428692d1c2a80d57c62266cdf3
rozetko <rozetko@corpglory.com>
2019-09-11 23:19:22 +0300

    it works

--- server/build/node-loader.js
+++ server/build/node-loader.js
@@ -2,11 +2,15 @@ var path = require('path');
 
 // based on: https://github.com/webpack-contrib/node-loader/blob/master/index.js
 module.exports = function nodeLoader(m, q) {
+  const ZERO_MQ_MODULE_PATH = `__dirname + '/${path.basename(this.resourcePath)}'`;
+  const DEASYNC_MODULE_PATH = `'${this.resourcePath}'`;
+  const isZeromq = this.resourcePath.indexOf('zeromq') >= 0;
   return (`
-    var modulePath = __dirname + '/${path.basename(this.resourcePath)}';
+    var modulePath = ${isZeromq? ZERO_MQ_MODULE_PATH: DEASYNC_MODULE_PATH};
     try {
       global.process.dlopen(module, modulePath); 
     } catch(e) {
+      console.error('dlopen: Cannot open ' + modulePath + ': ' + e);
       throw new Error('dlopen: Cannot open ' + modulePath + ': ' + e);
     }
   `);
--- server/build/webpack.prod.conf.js
+++ server/build/webpack.prod.conf.js
@@ -1,5 +1,25 @@
+const EventHooksPlugin = require('event-hooks-webpack-plugin');
+
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs-extra');
+
 var base = require('./webpack.base.conf');
 
+
+const NODE_VERSION = 'node-' + /[0-9]+/.exec(process.versions.node)[0];
+const PLATFORM = process.platform + '-' + process.arch + '-' + NODE_VERSION;
+const DEASYNC_DIST_PATH = path.join(
+  path.resolve(`dist`),
+  'bin',
+  PLATFORM
+);
+const DEASYNC_NODE_MODULES_PATH = path.join(
+  path.resolve('node_modules'),
+  'deasync',
+  'bin',
+  PLATFORM
+);
 
 base.mode = 'production';
 
@@ -10,6 +30,37 @@ base.module.rules.push({
     { loader: 'file-loader', options: { name: '[name].[ext]' } },
   ]
 });
+
+base.plugins.push(
+  new EventHooksPlugin({
+    beforeRun: () => {
+      const isPlatformSupported = fs.existsSync(DEASYNC_NODE_MODULES_PATH);
+      if(!isPlatformSupported) {
+        throw new Error(`deasync doesn't support this platform: ${PLATFORM}`);
+      }
+
+      console.log('Copying deasync.node from node_modules to dist/bin...');
+
+      fs.ensureDirSync(DEASYNC_DIST_PATH);
+
+      fs.copySync(
+        path.join(DEASYNC_NODE_MODULES_PATH, 'deasync.node'),
+        path.join(DEASYNC_DIST_PATH, 'deasync.node')
+      );
+    }
+  })
+);
+
+const deasyncBinFileName = path.join(DEASYNC_DIST_PATH, 'deasync');
+
+let pathMapping = {};
+pathMapping[deasyncBinFileName] = './deasync.node';
+
+base.plugins.push(new webpack.ContextReplacementPlugin(
+  /deasync/,
+  DEASYNC_DIST_PATH,
+  pathMapping
+));
 
 module.exports = base;
 
