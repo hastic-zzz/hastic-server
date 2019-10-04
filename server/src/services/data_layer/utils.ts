faa1780296d0c6af4e5bd69ef934a2084bab768d
Alexandr Velikiy <39257464+VargBurz@users.noreply.github.com>
2019-10-04 14:40:31 +0300

    Notification problem with mongodb #780 (#782)
    * some logs
    
    * fix findOne
    
    * fix
    
    * fix
    
    * fix2
    
    * fix3

--- server/src/services/data_layer/utils.ts
+++ server/src/services/data_layer/utils.ts
@@ -1,4 +1,4 @@
-import { ObjectID } from 'mongodb';
+import { FilterQuery, ObjectID } from 'mongodb';
 
 //TODO: move to DbQueryWrapper
 
@@ -9,9 +9,12 @@ export function wrapIdToQuery(query: string | object): object {
   return query;
 }
 
-export function wrapIdToMongoDbQuery(query: string | object): object {
+export function wrapIdToMongoDbQuery(query: FilterQuery<string | object>): object {
   if(typeof query === 'string') {
     return { _id: new ObjectID(query) };
+  }
+  if(typeof query._id === 'string') {
+    return { _id: new ObjectID(query._id) };
   }
   return query;
 }
