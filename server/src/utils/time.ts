01fae25acf1e0091e114f2095866e4cf810a2c20
VargBurz <voidstone1@mail.ru>
2019-10-07 12:52:13 +0300

    add utc

--- server/src/utils/time.ts
+++ server/src/utils/time.ts
@@ -4,7 +4,7 @@ import * as _ from 'lodash';
 import * as moment from 'moment';
 
 const MINUTES_IN_HOUR = 60;
-const TIME_FORMAT = 'ddd MMM DD YYYY HH:mm:ss Z';
+const TIME_FORMAT = 'ddd MMM DD YYYY HH:mm:ss UTCZ';
 
 export function parseTimeZone(timeZone: string): number {
   timeZone = timeZone.replace(/['|"]/g, '');
